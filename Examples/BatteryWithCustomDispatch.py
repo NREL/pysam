5e2f916c84817d47f875663249ca576f33a020ea
Brian Mirletz <brian.mirletz@nrel.gov>
2020-04-29 09:41:44 -0600

    clarify needs of subhourly simulations and flesh out comments

--- Examples/BatteryWithCustomDispatch.py
+++ Examples/BatteryWithCustomDispatch.py
@@ -1,6 +1,6 @@
 """
 This example script shows how to set up a stand alone battery with custom dispatch using a default battery model
-Any list of length analysis_period * hours_in_year can be used for dispatch
+Any list of floats with length analysis_period * steps_in_year can be used for dispatch
 
 Additional financial models, inputs, and outputs can be found at https://nrel-pysam.readthedocs.io/en/2.0.2/modules/StandAloneBattery.html
 
@@ -13,15 +13,16 @@ import PySAM.StandAloneBattery as battery_model
 from PySAM.PySSC import *
 
 analysis_period = 1 # years
-hours_in_year = 8760
+steps_in_year = 8760 # currently hours in year, multiply this for subhourly tests (example * 12 for 5 minute tests)
 days_in_year = 365
 
-# Create the model
+# Create the model using PySAM's defaults
 battery = battery_model.default("GenericSystemSingleOwner")
 
-# Set up inputs needed by the model
-battery.BatteryCell.batt_room_temperature_celsius = [20] * (hours_in_year * analysis_period) # degrees C. Would normally come from weather file
+# Set up inputs needed by the model.
+battery.BatteryCell.batt_room_temperature_celsius = [20] * (steps_in_year * analysis_period) # degrees C, room temperature. Would normally come from weather file
 
+# 24 hours of data to duplicate for the test. Would need to add data here for subhourly
 lifetime_generation = []
 lifetime_dispatch = []
 daily_generation = [0, 0, 0, 0, 0, 0, 0, 200, 400, 600, 800, 1000, 1000, 1000, 1000, 800, 600, 400, 200, 0, 0, 0, 0, 0] # kW
@@ -35,13 +36,14 @@ for i in range(0, days_in_year * analysis_period):
 # Normally output from pvsamv1, need to set up custom system generation here
 battery.SystemOutput.gen = lifetime_generation # converts list to tuple
 
+# Change from default dispatch to custom dispatch
 battery.BatteryDispatch.batt_dispatch_choice = 3 # custom dispatch
 battery.BatteryDispatch.batt_custom_dispatch = lifetime_dispatch
     
-# Run the model    
+# Run the model. Change argument to 1 for verbose    
 battery.execute(0)
 
-# Export outputs to a dictionary
+# Export outputs to a dictionary. All outputs on readthedocs page are exported
 output = battery.export()
 
 print("Roundtrip efficiency: " + str(output["Outputs"]["average_battery_roundtrip_efficiency"]))
