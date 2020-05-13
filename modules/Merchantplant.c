c27debb73da272cfa03394709cf9b9574e7b532f
dguittet <dguittet@nrel.gov>
2020-05-13 11:15:23 -0600

    update merchant plant types & docs

--- modules/Merchantplant.c
+++ modules/Merchantplant.c
@@ -3979,6 +3979,18 @@ Revenue_set_mp_ancserv3_revenue(VarGroupObject *self, PyObject *value, void *clo
 }
 
 static PyObject *
+Revenue_get_mp_ancserv4_revenue(VarGroupObject *self, void *closure)
+{
+	return PySAM_matrix_getter(SAM_Merchantplant_Revenue_mp_ancserv4_revenue_mget, self->data_ptr);
+}
+
+static int
+Revenue_set_mp_ancserv4_revenue(VarGroupObject *self, PyObject *value, void *closure)
+{
+		return PySAM_matrix_setter(value, SAM_Merchantplant_Revenue_mp_ancserv4_revenue_mset, self->data_ptr);
+}
+
+static PyObject *
 Revenue_get_mp_enable_ancserv1(VarGroupObject *self, void *closure)
 {
 	return PySAM_double_getter(SAM_Merchantplant_Revenue_mp_enable_ancserv1_nget, self->data_ptr);
@@ -4038,6 +4050,18 @@ Revenue_set_mp_enable_energy_market_revenue(VarGroupObject *self, PyObject *valu
 	return PySAM_double_setter(value, SAM_Merchantplant_Revenue_mp_enable_energy_market_revenue_nset, self->data_ptr);
 }
 
+static PyObject *
+Revenue_get_mp_energy_market_revenue(VarGroupObject *self, void *closure)
+{
+	return PySAM_matrix_getter(SAM_Merchantplant_Revenue_mp_energy_market_revenue_mget, self->data_ptr);
+}
+
+static int
+Revenue_set_mp_energy_market_revenue(VarGroupObject *self, PyObject *value, void *closure)
+{
+		return PySAM_matrix_setter(value, SAM_Merchantplant_Revenue_mp_energy_market_revenue_mset, self->data_ptr);
+}
+
 static PyGetSetDef Revenue_getset[] = {
 {"flip_target_percent", (getter)Revenue_get_flip_target_percent,(setter)Revenue_set_flip_target_percent,
 	PyDoc_STR("*float*: After-tax IRR target [%]\n\n*Constraints*: MIN=0,MAX=100\n\n*Required*: If not provided, assumed to be 11"),
@@ -4046,28 +4070,34 @@ static PyGetSetDef Revenue_getset[] = {
 	PyDoc_STR("*float*: IRR target year [Year]\n\n*Constraints*: MIN=1\n\n*Required*: If not provided, assumed to be 11"),
  	NULL},
 {"mp_ancserv1_revenue", (getter)Revenue_get_mp_ancserv1_revenue,(setter)Revenue_set_mp_ancserv1_revenue,
-	PyDoc_STR("*sequence[sequence]*: Ancillary services 1 revenue input\n\n*Required*: True"),
+	PyDoc_STR("*sequence[sequence]*: Ancillary services 1 revenue input\n\n*Info*: Lifetime x 2[Cleared Capacity(MW),Price($/MWh)]\n\n*Required*: True"),
  	NULL},
 {"mp_ancserv2_revenue", (getter)Revenue_get_mp_ancserv2_revenue,(setter)Revenue_set_mp_ancserv2_revenue,
-	PyDoc_STR("*sequence[sequence]*: Ancillary services 2 revenue input\n\n*Required*: True"),
+	PyDoc_STR("*sequence[sequence]*: Ancillary services 2 revenue input\n\n*Info*: Lifetime x 2[Cleared Capacity(MW),Price($/MWh)]\n\n*Required*: True"),
  	NULL},
 {"mp_ancserv3_revenue", (getter)Revenue_get_mp_ancserv3_revenue,(setter)Revenue_set_mp_ancserv3_revenue,
-	PyDoc_STR("*sequence[sequence]*: Ancillary services 3 revenue input\n\n*Required*: True"),
+	PyDoc_STR("*sequence[sequence]*: Ancillary services 3 revenue input\n\n*Info*: Lifetime x 2 [Cleared Capacity(MW),Price($/MWh)]\n\n*Required*: True"),
+ 	NULL},
+{"mp_ancserv4_revenue", (getter)Revenue_get_mp_ancserv4_revenue,(setter)Revenue_set_mp_ancserv4_revenue,
+	PyDoc_STR("*sequence[sequence]*: Ancillary services 4 revenue input\n\n*Info*: Lifetime x 2 [Cleared Capacity(MW),Price($/MWh)]\n\n*Required*: True"),
  	NULL},
 {"mp_enable_ancserv1", (getter)Revenue_get_mp_enable_ancserv1,(setter)Revenue_set_mp_enable_ancserv1,
-	PyDoc_STR("*float*: Enable ancillary services 1 revenue [0/1]\n\n*Constraints*: INTEGER,MIN=0,MAX=1\n\n*Required*: True"),
+	PyDoc_STR("*float*: Enable ancillary services 1 Revenue [0/1]\n\n*Constraints*: INTEGER,MIN=0,MAX=1\n\n*Required*: True"),
  	NULL},
 {"mp_enable_ancserv2", (getter)Revenue_get_mp_enable_ancserv2,(setter)Revenue_set_mp_enable_ancserv2,
-	PyDoc_STR("*float*: Enable ancillary services 2 revenue [0/1]\n\n*Constraints*: INTEGER,MIN=0,MAX=1\n\n*Required*: True"),
+	PyDoc_STR("*float*: Enable ancillary services 2 Revenue [0/1]\n\n*Constraints*: INTEGER,MIN=0,MAX=1\n\n*Required*: True"),
  	NULL},
 {"mp_enable_ancserv3", (getter)Revenue_get_mp_enable_ancserv3,(setter)Revenue_set_mp_enable_ancserv3,
-	PyDoc_STR("*float*: Enable ancillary services 3 revenue [0/1]\n\n*Constraints*: INTEGER,MIN=0,MAX=1\n\n*Required*: True"),
+	PyDoc_STR("*float*: Enable ancillary services 3 Revenue [0/1]\n\n*Constraints*: INTEGER,MIN=0,MAX=1\n\n*Required*: True"),
  	NULL},
 {"mp_enable_ancserv4", (getter)Revenue_get_mp_enable_ancserv4,(setter)Revenue_set_mp_enable_ancserv4,
-	PyDoc_STR("*float*: Enable ancillary services 4 revenue [0/1]\n\n*Constraints*: INTEGER,MIN=0,MAX=1\n\n*Required*: True"),
+	PyDoc_STR("*float*: Enable ancillary services 4 Revenue [0/1]\n\n*Constraints*: INTEGER,MIN=0,MAX=1\n\n*Required*: True"),
  	NULL},
 {"mp_enable_energy_market_revenue", (getter)Revenue_get_mp_enable_energy_market_revenue,(setter)Revenue_set_mp_enable_energy_market_revenue,
 	PyDoc_STR("*float*: Enable energy market revenue [0/1]\n\n*Constraints*: INTEGER,MIN=0,MAX=1\n\n*Required*: True"),
+ 	NULL},
+{"mp_energy_market_revenue", (getter)Revenue_get_mp_energy_market_revenue,(setter)Revenue_set_mp_energy_market_revenue,
+	PyDoc_STR("*sequence[sequence]*: Energy market revenue input\n\n*Info*: Lifetime x 2[Cleared Capacity(MW),Price($/MWh)]\n\n*Required*: True"),
  	NULL},
 	{NULL}  /* Sentinel */
 };
@@ -4743,139 +4773,6 @@ static PyTypeObject Lifetime_Type = {
 		Lifetime_methods,         /*tp_methods*/
 		0,                          /*tp_members*/
 		Lifetime_getset,          /*tp_getset*/
-		0,                          /*tp_base*/
-		0,                          /*tp_dict*/
-		0,                          /*tp_descr_get*/
-		0,                          /*tp_descr_set*/
-		0,                          /*tp_dictofnset*/
-		0,                          /*tp_init*/
-		0,                          /*tp_alloc*/
-		0,             /*tp_new*/
-		0,                          /*tp_free*/
-		0,                          /*tp_is_gc*/
-};
-
-
-/*
- * Market Group
- */ 
-
-static PyTypeObject Market_Type;
-
-static PyObject *
-Market_new(SAM_Merchantplant data_ptr)
-{
-	PyObject* new_obj = Market_Type.tp_alloc(&Market_Type,0);
-
-	VarGroupObject* Market_obj = (VarGroupObject*)new_obj;
-
-	Market_obj->data_ptr = (SAM_table)data_ptr;
-
-	return new_obj;
-}
-
-/* Market methods */
-
-static PyObject *
-Market_assign(VarGroupObject *self, PyObject *args)
-{
-	PyObject* dict;
-	if (!PyArg_ParseTuple(args, "O:assign", &dict)){
-		return NULL;
-	}
-
-	if (!PySAM_assign_from_dict(self->data_ptr, dict, "Merchantplant", "Market")){
-		return NULL;
-	}
-
-	Py_INCREF(Py_None);
-	return Py_None;
-}
-
-static PyObject *
-Market_export(VarGroupObject *self, PyObject *args)
-{
-	PyTypeObject* tp = &Market_Type;
-	PyObject* dict = PySAM_export_to_dict((PyObject *) self, tp);
-	return dict;
-}
-
-static PyMethodDef Market_methods[] = {
-		{"assign",            (PyCFunction)Market_assign,  METH_VARARGS,
-			PyDoc_STR("assign() -> None\n Assign attributes from dictionary\n\n``Market_vals = { var: val, ...}``")},
-		{"export",            (PyCFunction)Market_export,  METH_VARARGS,
-			PyDoc_STR("export() -> dict\n Export attributes into dictionary")},
-		{NULL,              NULL}           /* sentinel */
-};
-
-static PyObject *
-Market_get_mp_ancserv4_revenue(VarGroupObject *self, void *closure)
-{
-	return PySAM_matrix_getter(SAM_Merchantplant_Market_mp_ancserv4_revenue_mget, self->data_ptr);
-}
-
-static int
-Market_set_mp_ancserv4_revenue(VarGroupObject *self, PyObject *value, void *closure)
-{
-		return PySAM_matrix_setter(value, SAM_Merchantplant_Market_mp_ancserv4_revenue_mset, self->data_ptr);
-}
-
-static PyObject *
-Market_get_mp_energy_market_revenue(VarGroupObject *self, void *closure)
-{
-	return PySAM_matrix_getter(SAM_Merchantplant_Market_mp_energy_market_revenue_mget, self->data_ptr);
-}
-
-static int
-Market_set_mp_energy_market_revenue(VarGroupObject *self, PyObject *value, void *closure)
-{
-		return PySAM_matrix_setter(value, SAM_Merchantplant_Market_mp_energy_market_revenue_mset, self->data_ptr);
-}
-
-static PyGetSetDef Market_getset[] = {
-{"mp_ancserv4_revenue", (getter)Market_get_mp_ancserv4_revenue,(setter)Market_set_mp_ancserv4_revenue,
-	PyDoc_STR("*sequence[sequence]*: Ancillary services 4 revenue input\n\n*Required*: True"),
- 	NULL},
-{"mp_energy_market_revenue", (getter)Market_get_mp_energy_market_revenue,(setter)Market_set_mp_energy_market_revenue,
-	PyDoc_STR("*sequence[sequence]*: Energy market revenue input\n\n*Required*: True"),
- 	NULL},
-	{NULL}  /* Sentinel */
-};
-
-static PyTypeObject Market_Type = {
-		/* The ob_type field must be initialized in the module init function
-		 * to be portable to Windows without using C++. */
-		PyVarObject_HEAD_INIT(NULL, 0)
-		"Merchantplant.Market",             /*tp_name*/
-		sizeof(VarGroupObject),          /*tp_basicsize*/
-		0,                          /*tp_itemsize*/
-		/* methods */
-		0,    /*tp_dealloc*/
-		0,                          /*tp_print*/
-		(getattrfunc)0,             /*tp_getattr*/
-		0,                          /*tp_setattr*/
-		0,                          /*tp_reserved*/
-		0,                          /*tp_repr*/
-		0,                          /*tp_as_number*/
-		0,                          /*tp_as_sequence*/
-		0,                          /*tp_as_mapping*/
-		0,                          /*tp_hash*/
-		0,                          /*tp_call*/
-		0,                          /*tp_str*/
-		0,                          /*tp_getattro*/
-		0,                          /*tp_setattro*/
-		0,                          /*tp_as_buffer*/
-		Py_TPFLAGS_DEFAULT,         /*tp_flags*/
-		0,                          /*tp_doc*/
-		0,                          /*tp_traverse*/
-		0,                          /*tp_clear*/
-		0,                          /*tp_richcompare*/
-		0,                          /*tp_weaklistofnset*/
-		0,                          /*tp_iter*/
-		0,                          /*tp_iternext*/
-		Market_methods,         /*tp_methods*/
-		0,                          /*tp_members*/
-		Market_getset,          /*tp_getset*/
 		0,                          /*tp_base*/
 		0,                          /*tp_dict*/
 		0,                          /*tp_descr_get*/
@@ -9902,10 +9799,6 @@ newMerchantplantObject(void* data_ptr)
 	PyDict_SetItemString(attr_dict, "Lifetime", Lifetime_obj);
 	Py_DECREF(Lifetime_obj);
 
-	PyObject* Market_obj = Market_new(self->data_ptr);
-	PyDict_SetItemString(attr_dict, "Market", Market_obj);
-	Py_DECREF(Market_obj);
-
 	PyObject* FuelCell_obj = FuelCell_new(self->data_ptr);
 	PyDict_SetItemString(attr_dict, "FuelCell", FuelCell_obj);
 	Py_DECREF(FuelCell_obj);
@@ -10244,13 +10137,6 @@ MerchantplantModule_exec(PyObject *m)
 				"Lifetime",
 				(PyObject*)&Lifetime_Type);
 	Py_DECREF(&Lifetime_Type);
-
-	/// Add the Market type object to Merchantplant_Type
-	if (PyType_Ready(&Market_Type) < 0) { goto fail; }
-	PyDict_SetItemString(Merchantplant_Type.tp_dict,
-				"Market",
-				(PyObject*)&Market_Type);
-	Py_DECREF(&Market_Type);
 
 	/// Add the FuelCell type object to Merchantplant_Type
 	if (PyType_Ready(&FuelCell_Type) < 0) { goto fail; }
