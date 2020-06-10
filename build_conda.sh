4fee0048a04cde1e67d8ea093c15cdd0968d49a0
dguittet <dguittet@nrel.gov>
2020-06-10 15:08:08 -0600

    fix build_conda order of stubs

--- build_conda.sh
+++ build_conda.sh
@@ -3,6 +3,16 @@
 cd $PYSAMDIR || exit
 source $(conda info --base)/etc/profile.d/conda.sh
 
+DIST_NAME=''
+if [ `uname` = "Linux" ] ;
+then
+    DIST_NAME='linux-64'
+elif [ `uname` = "Darwin" ] ;
+then
+    DIST_NAME='osx-64'
+fi
+rm -rf dist/$DIST_NAME
+
 # read version
 VERSION="$(python -c 'from files.version import __version__; print(__version__)' 2>&1)"
 export VERSION
@@ -11,7 +21,15 @@ yes | conda install conda-build
 conda activate base
 for PYTHONVER in 3.5 3.6 3.7 3.8
 do
-   conda-build conda --output-folder=dist --python=$PYTHONVER || exit
    conda-build stubs --output-folder=dist --python=$PYTHONVER || exit
    conda build purge
 done
+anaconda upload -u nrel dist/$DIST_NAME/*pysam-stubs*.bz2 || exit
+rm -rf dist/$DIST_NAME
+
+for PYTHONVER in 3.5 3.6 3.7 3.8
+do
+   conda-build conda --output-folder=dist --python=$PYTHONVER || exit
+   conda build purge
+done
+anaconda upload -u nrel dist/$DIST_NAME/*pysam*.bz2
