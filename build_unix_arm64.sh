5f4cac12bfc2a14d0c78d5b587e8f29e9942b531
sjanzou <sjanzou@gmail.com>
2022-09-09 05:04:18 -0600

    Start on shell scripts for arm64 Mac wheel and aarch64 linux wheels

---
+++ build_unix_arm64.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+#
+# Building for Mac
+#
+
+# Building libssc and libSAM_api
+# requires SAM-Dev/CMakeList.txt that contains lk, wex, ssc and sam as subdirectories
+
+mkdir -p ~/NREL/cmake-build-release
+cd ~/NREL/cmake-build-release || exit
+cmake .. -DCMAKE_BUILD_TYPE=Release -DSAMAPI_EXPORT=1 -DSAM_SKIP_AUTOGEN=0 
+cmake --build . --target SAM_api -j 6
+
+# Building the PyPi and Anaconda packages
+# requires Anaconda installed with an environment per Python version from 3.5 to 3.8
+# named pysam_build_3.5 pysam_build_3.6 pysam_build_3.7 pysam_build_3.8
+
+
+cd $PYSAMDIR || exit
+source $(conda info --base)/etc/profile.d/conda.sh
+rm -rf build
+rm -rf dist/*
+
+for PYTHONENV in pysam_build_3.9
+do
+   conda activate $PYTHONENV
+   yes | pip install -r tests/requirements.txt
+   yes | pip uninstall NREL-PySAM
+   python setup.py install || exit
+   pytest -s tests
+   retVal=$?
+   if [ $retVal -ne 0 ]; then
+       echo "Error in Tests"
+       exit 1
+   fi
+   python setup.py bdist_wheel
+done
+
+yes | $PYSAMDIR/build_conda_arm64.sh || exit
+
+#
+# Building for Manylinux1
+#
+
+cd ..
+docker pull quay.io/pypa/manylinux2014_aarch64
+# docker run --rm -dit -v $(pwd):/io quay.io/pypa/manylinux2010_x86_64 /bin/bash
+docker run --rm -v $(pwd):/io quay.io/pypa/manylinux2014_aarch64 /io/pysam/build_manylinux.sh
+
+rename -s linux manylinux1 $PYSAMDIR/dist/*-linux_*
+
+docker pull continuumio/anaconda
+docker run --rm --env PYSAMDIR=/io/pysam -v $(pwd):/io continuumio/anaconda /io/pysam/build_conda_arm64.sh
+
+
+#twine upload $PYSAMDIR/dist/*.whl
+#anaconda upload -u nrel $PYSAMDIR/dist/osx-64/*.tar.bz2
+#anaconda upload -u nrel $PYSAMDIR/dist/linux-64/*.tar.bz2
+
