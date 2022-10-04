947f5889ba327199418057c6571d91d41e194126
dguittet <dguittet@nrel.gov>
2022-10-04 14:24:25 -0600

    update comments

--- build_unix.sh
+++ build_unix.sh
@@ -14,8 +14,8 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -DSAMAPI_EXPORT=1 -DSAM_SKIP_AUTOGEN=0
 cmake --build . --target SAM_api -j 6
 
 # Building the PyPi and Anaconda packages
-# requires Anaconda installed with an environment per Python version from 3.5 to 3.8
-# named pysam_build_3.5 pysam_build_3.6 pysam_build_3.7 pysam_build_3.8
+# requires Anaconda installed with an environment per Python version from 3.6 to 3.10
+# named pysam_build_3.6 pysam_build_3.7 pysam_build_3.8, etc
 
 
 cd $PYSAMDIR || exit
