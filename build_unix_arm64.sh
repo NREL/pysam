5220473524bbfec15957aadc7924f5e859098264
sjanzou <steven@janzouconsulting.com>
2022-12-12 09:15:30 -0700

    arm64 and aarch64 shell scripts (#130)
    * files for pysam 2.2.4
    
    * update build_manylinux.sh
    
    * update tests and conda build
    
    * update SAM_api files
    
    * closes #89 certifi certificates in fetchresource
    
    * fixes #94
    
    * start v 2.3.0
    
    * fix #69
    
    * changes from 2021.12.2 release
    
    * add VersionChanges.rst
    
    * add version_differ.py
    
    * add more release info
    
    * update version_differ.py doc str
    
    * correct version_differ.py with PySSC
    
    * add python 3.10
    
    * fix getting env variables during build and remove pandas dependency
    
    * fix test_ResouceTools.py
    
    * switch to manylinux2010 for Python 3.10 support
    
    * update pysam files
    
    * update PySAM files
    
    * Update docs for Version 3.0.1
    
    * close #109
    
    * remove stubs package dependency
    
    * removal of stubs package
    
    * Apple Silicon and Intel Wheel
    
    pip3 install --extra-index-url https://test.pypi.org/simple/ NREL_PySAM
    
    * Latest develop branches of ssc and SAM - maybe after v3.0.2
    
    * Start on shell scripts for arm64 Mac wheel and aarch64 linux wheels
    
    * building arm64 python 3.8 on apple silicon
    
    * remove .zip files from commit
    
    * PySAM 4.0.0 for Apple Silicon
    
    * Apple Silicon arm64 and manylinux aarch64 shell files for building wheels
    
    Co-authored-by: dguittet <dguittet@nrel.gov>

---
+++ build_unix_arm64.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+#
+# Building for Mac
+#
+export ROOTDIR=/Users/imacuser/Public/Projects/GitHub/NREL
+export LKDIR=/Users/imacuser/Public/Projects/GitHub/NREL/lk
+export WEXDIR=/Users/imacuser/Public/Projects/GitHub/NREL/wex
+export SSCDIR=/Users/imacuser/Public/Projects/GitHub/NREL/ssc
+export SAMNTDIR=/Users/imacuser/Public/Projects/GitHub/NREL/SAM
+export PYSAMDIR=/Users/imacuser/Public/Projects/GitHub/NREL/pysam
+
+
+# Building libssc and libSAM_api
+# requires $ROOTDIR/CMakeList.txt that contains lk, wex, ssc and sam as subdirectories
+
+mkdir -p $ROOTDIR/cmake-build-release
+cd $ROOTDIR/cmake-build-release || exit
+cmake .. -DCMAKE_BUILD_TYPE=Release -DSAMAPI_EXPORT=1 -DSAM_SKIP_AUTOGEN=0 
+cmake --build . --target SAM_api -j 
+
+# Building the PyPi and Anaconda packages
+# requires Anaconda installed with an environment per Python version from 3.8 to 3.10 (arm64 support for apple silicon)
+# named pysam_build_3.8 pysam_build_3.9 pysam_build_3.10
+
+
+cd $PYSAMDIR || exit
+source $(conda info --base)/etc/profile.d/conda.sh
+rm -rf build
+rm -rf dist/*
+
+for PYTHONENV in pysam_build_3.8 pysam_build_3.9 pysam_build_3.10
+do
+   conda activate $PYTHONENV
+   yes | pip install -r tests/requirements.txt
+   yes | pip uninstall NREL-PySAM
+   python setup_arm64.py install || exit
+   pytest -s tests
+   retVal=$?
+   if [ $retVal -ne 0 ]; then
+       echo "Error in Tests"
+       exit 1
+   fi
+   python setup_arm64.py bdist_wheel
+done
+
+yes | $PYSAMDIR/build_conda_arm64.sh || exit
+
+#
+# Building for Manylinux1
+#
+
+cd ..
+docker pull quay.io/pypa/manylinux2014_aarch64
+# docker run --rm -dit -v $(pwd):/io quay.io/pypa/manylinux2010_x86_64 /bin/bash
+docker run --rm -v $(pwd):/io quay.io/pypa/manylinux2014_aarch64 /io/pysam/build_manylinux_arm64.sh
+
+rename -s linux manylinux1 $PYSAMDIR/dist/*-linux_*
+
+docker pull continuumio/anaconda
+docker run --rm --env PYSAMDIR=/io/pysam -v $(pwd):/io continuumio/anaconda /io/pysam/build_conda_arm64.sh
+
+
+#twine upload $PYSAMDIR/dist/*.whl
+#anaconda upload -u nrel $PYSAMDIR/dist/osx-64/*.tar.bz2
+#anaconda upload -u nrel $PYSAMDIR/dist/linux-64/*.tar.bz2
+
