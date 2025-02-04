f7296113894d90825c7e8cef51d36ea10584b41d
Matt Prilliman <54449384+mjprilliman@users.noreply.github.com>
2025-02-03 19:54:25 -0600

    Add test coverage for pvsamv1 tools

---
+++ tests/test_Pvsamv1Tools.py
@@ -0,0 +1,58 @@
+import pytest
+
+import PySAM.Pvsamv1Tools as PVTools
+import PySAM.Pvsamv1 as pvsamv1
+import PySAM.BatteryStateful as battstfl
+
+
+def test_default_system_sizing():
+    model = pvsamv1.default("FlatPlatePVSingleOwner")
+    model.SolarResource.solar_resource_file = 'C://Users//mprillim//sam_dev//SAM//deploy//solar_resource//phoenix_az_33.450495_-111.983688_psmv3_60_tmy.csv'
+    model.value('analysis_period', 1)
+    n_strings, n_combiners, n_inverters, calculated_system_capacity = PySAM.Pvsamv1Tools.size_electrical_parameters(model, 130000, 1.2)
+    assert(n_strings == 8748)
+    assert(n_inverters == 43)
+    assert(calculated_system_capacity == pytest.approx(130004,1))
+    assert(model.SystemDesign.subarray1_modules_per_string == 28)
+    assert(model.SystemDesign.system_capacity == pytest.approx(130004, 1))
+
+def test_spe_system_sizing():
+    model = pvsamv1.default("FlatPlatePVSingleOwner")
+    model.SolarResource.solar_resource_file = 'C://Users//mprillim//sam_dev//SAM//deploy//solar_resource//phoenix_az_33.450495_-111.983688_psmv3_60_tmy.csv'
+    model.value('module_model', 0) #SPE Model
+    model.value('analysis_period', 1)
+    n_strings, n_combiners, n_inverters, calculated_system_capacity = PySAM.Pvsamv1Tools.size_electrical_parameters(model, 130000, 1.2)
+    assert(n_strings == 8778)
+    assert(n_inverters == 43)
+    assert(calculated_system_capacity == pytest.approx(129995))
+    assert(model.SystemDesign.subarray1_modules_per_string == 28)
+    assert(model.SystemDesign.system_capacity == pytest.approx(129995, 1))
+
+def test_6par_system_sizing():
+    model = pvsamv1.default("FlatPlatePVSingleOwner")
+    model.value('analysis_period', 1)
+    n_strings, n_combiners, n_inverters, calculated_system_capacity = PySAM.Pvsamv1Tools.size_electrical_parameters(model, 130000, 1.2)
+    assert(n_strings == 8748)
+    assert(n_inverters == 43)
+    assert(calculated_system_capacity == pytest.approx(130004,1))
+    assert(model.SystemDesign.subarray1_modules_per_string == 28)
+    assert(model.SystemDesign.system_capacity == pytest.approx(130004, 1))
+
+def test_cec_module_library_selection():
+    model = pvsamv1.default("FlatPlatePVSingleOwner")
+    mod_dict = PySAM.Pvsamv1Tools.set_cec_module_library_selection(model, "AIMS Power PV555MONO")
+    assert(model.value("cec_v_mp_ref"), 43.3)
+    assert(model.value('cec_i_mp_ref'), 12.81)
+    assert(model.value('cec_v_oc_ref'), 49.9)
+    assert(model.value('cec_i_sc_ref'), 13.74)
+
+def test_cec_inverter_library_selection():
+    model = pvsamv1.default("FlatePlatePVSingleOwner")
+    inv_dict = PySAM.Pvsamv1Tools.set_cec_inverter_library_selection(model, "NEXTRACKER LLC: NXPCS3340K-HUD [800V]")
+    assert(model.value('inv_snl_paco'), 3347800)
+    assert(model.value('inv_snl_mppt_hi'), 1500)
+    assert(model.value('inv_snl_mppt_low'), 1200)
+    assert(model.value('inv_snl_vdcmax'), 1500)
+
+
+
