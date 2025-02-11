4d0169142043142b9a961da04e69cbc942b5ae30
Paul Gilman <cpaulgilman@gmail.com>
2025-02-11 12:17:33 -0800

    Fix issue with value() not working
    Plus a couple of typos

--- files/Pvsamv1Tools.py
+++ files/Pvsamv1Tools.py
@@ -49,7 +49,7 @@ def find_modules_per_string(
     if inv_vdcmax > 0:
         while modules_per_string > 0 and modules_per_string * v_oc_module > inv_vdcmax:
             modules_per_string -= 1
-    model.value('subarray1_modules_per_string', modules_per_string)
+    model.SystemDesign.subarray1_modules_per_string = modules_per_string
     return modules_per_string
 
 
@@ -75,7 +75,7 @@ def find_inverter_count(
     """
     n_inverters_frac = modules_per_string * n_strings * module_power / (dc_ac_ratio * inverter_power)
     n_inverters = max(1, round(n_inverters_frac))
-    model.value('inverter_count', n_inverters)
+    model.SystemDesign.inverter_count = n_inverters
     return n_inverters
 
 
@@ -101,69 +101,71 @@ def size_electrical_parameters(
     """
     
 
-    module_model = model.value('module_model')
+    module_model = model.Module.module_model
     module_power = 0
     module_vmp = 0
     module_voc = 0
     match module_model:
         case 0:
-            ref = model.value("spe_reference")
-            eff = model.value("spe_eff{}".format(ref))
-            rad = model.value("spe_eff{}".format(ref))
-            area = model.value("spe_area")
+            ref = int(model.SimpleEfficiencyModuleModel.spe_reference)
+            eff_list = [model.SimpleEfficiencyModuleModel.spe_eff0, model.SimpleEfficiencyModuleModel.spe_eff1, model.SimpleEfficiencyModuleModel.spe_eff2, model.SimpleEfficiencyModuleModel.spe_eff3, model.SimpleEfficiencyModuleModel.spe_eff4]
+            rad_list = [model.SimpleEfficiencyModuleModel.spe_rad0, model.SimpleEfficiencyModuleModel.spe_rad1, model.SimpleEfficiencyModuleModel.spe_rad2, model.SimpleEfficiencyModuleModel.spe_rad3, model.SimpleEfficiencyModuleModel.spe_rad4]
+            eff = eff_list[ref]
+            rad = rad_list[ref]
+            area = model.SimpleEfficiencyModuleModel.spe_area
             module_power = eff / 100.0 * rad * area #Wdc
-            module_vmp = model.value('spe_vmp')
-            module_voc = model.value('spe_voc')
+            module_vmp = model.SimpleEfficiencyModuleModel.spe_vmp
+            module_voc = model.SimpleEfficiencyModuleModel.spe_voc
         case 1:
-            module_power = model.value('cec_v_mp_ref') * model.value('cec_i_mp_ref')
-            module_vmp = model.value('cec_v_mp_ref')
-            module_voc = model.value('cec_v_oc_ref')
+            module_power = model.CECPerformanceModelWithModuleDatabase.cec_v_mp_ref * model.CECPerformanceModelWithModuleDatabase.cec_i_mp_ref
+            module_vmp = model.CECPerformanceModelWithModuleDatabase.cec_v_mp_ref
+            module_voc = model.CECPerformanceModelWithModuleDatabase.cec_v_oc_ref
         case 2:
-            module_vmp = model.value('6par_vmp')
-            module_voc = model.value('6par_voc')
-            module_power = module_vmp * model.value('6par_imp')
+            module_vmp = model.CECPerformanceModelWithUserEnteredSpecifications.sixpar_vmp
+            module_voc = model.CECPerformanceModelWithUserEnteredSpecifications.sixpar_voc
+            module_power = module_vmp * model.CECPerformanceModelWithUserEnteredSpecifications.sixpar_imp
         case 3:
             print('This function does not currently work for the Sandia Array Performance Model')
             '''
-            module_power = model.value('snl_ref_pmp')
-            module_vmp = model.value('snl_ref_vmp')
-            module_voc = model.value('snl_ref_voc')
+            module_power = model.SandiaPVArrayPerformanceModelWithModuleDatabase.snl_ref_pmp
+            module_vmp = model.SandiaPVArrayPerformanceModelWithModuleDatabase.snl_ref_vmp
+            module_voc = model.SandiaPVArrayPerformanceModelWithModuleDatabase.snl_ref_voc
             '''
         case 4:
-            module_vmp = model.value('sd11par_Vmp0')
-            module_voc = model.value('sd11par_Voc0')
-            module_power = module_vmp * model.value('sd11par_Imp0')
+            module_vmp = model.IEC61853SingleDiodeModel.sd11par_Vmp0
+            module_voc = model.IEC61853SingleDiodeModel.sd11par_Voc0
+            module_power = module_vmp * model.IEC61853SingleDiodeModel .sd11par_Imp0
         case 5:
-            module_vmp = model.value('mlm_V_mp_ref')
-            module_voc = model.value('mlm_V_oc_ref')
-            module_power = module_vmp * model.value('mlm_I_mp_ref')
+            module_vmp = model.MermoudLejeuneSingleDiodeModel.mlm_V_mp_ref
+            module_voc = model.MermoudLejeuneSingleDiodeModel.mlm_V_oc_ref
+            module_power = module_vmp * model.MermoudLejeuneSingleDiodeModel.mlm_I_mp_ref
 
-    inverter_model = model.value('inverter_model')
+    inverter_model = model.Inverter.inverter_model
     inv_power = 0
     vdcmax_inv = 0
     v_mppt_max = 0
     v_mppt_min = 0
     match inverter_model:
         case 0:
-            inv_power = model.value('inv_snl_paco') 
-            vdcmax_inv = model.value('inv_snl_vdcmax')
+            inv_power = model.InverterCECDatabase.inv_snl_paco
+            vdcmax_inv = model.InverterCECDatabase.inv_snl_vdcmax
         case 1:
-            inv_power = model.value('inv_ds_paco') 
-            vdcmax_inv = model.value('inv_ds_vdcmax')
+            inv_power = model.InverterDatasheet.inv_ds_paco
+            vdcmax_inv = model.InverterDatasheet.inv_ds_vdcmax
         case 2:
-            inv_power = model.value('inv_pd_paco')
-            vdcmax_inv = model.value('inv_pd_vdcmax')
+            inv_power = model.InverterPartLoadCurve.inv_pd_paco
+            vdcmax_inv = model.InverterPartLoadCurve.inv_pd_vdcmax
         case 3:
-            inv_power = model.value('inv_cec_cg_paco') 
-            vdcmax_inv = model.value('inv_cec_cg_vdcmax')
+            inv_power = model.InverterCECCoefficientGenerator.inv_cec_cg_paco
+            vdcmax_inv = model.InverterCECCoefficientGenerator.inv_cec_cg_vdcmax
         case 4:
-            inv_power = model.value('ond_PMaxOUT') 
-            vdcmax_inv = model.value('ond_VAbsMax')
+            inv_power = model.InverterMermoudLejeuneModel .ond_PMaxOUT
+            vdcmax_inv = model.InverterMermoudLejeuneModel .ond_VAbsMax
 
     if vdcmax_inverter is not None:
         vdcmax_inv = vdcmax_inverter
-    v_mppt_min = model.value('mppt_low_inverter')
-    v_mppt_max = model.value('mppt_hi_inverter')
+    v_mppt_min = model.Inverter.mppt_low_inverter
+    v_mppt_max = model.Inverter.mppt_hi_inverter
     modules_per_string = find_modules_per_string(model, v_mppt_min, v_mppt_max, module_vmp, module_voc, vdcmax_inv)
 
     n_strings_frac = target_system_capacity * 1000.0 / (modules_per_string * module_power) #Wac/Wdc
@@ -194,9 +196,9 @@ def size_electrical_parameters(
         modules_per_string=[modules_per_string],
         module_power=module_power / 1000.0 #kWac
     )
-    model.value('subarray1_modules_per_string', modules_per_string)
-    model.value('subarray1_nstrings', n_strings)
-    model.value('system_capacity', calculated_system_capacity)
+    model.SystemDesign.subarray1_modules_per_string = modules_per_string
+    model.SystemDesign.subarray1_nstrings = n_strings
+    model.SystemDesign.system_capacity = calculated_system_capacity
     return n_strings, n_combiners, n_inverters, calculated_system_capacity
 
 
@@ -269,16 +271,18 @@ def get_num_modules(pvsam_model) -> float:
     Return the number of modules in all subarrays
     """
     n_modules = 0
+    en_subarray_list = [ pvsam_model.SystemDesign.subarray1_enable, pvsam_model.SystemDesign.subarray2_enable, pvsam_model.SystemDesign.subarray3_enable, pvsam_model.SystemDesign.subarray4_enable ]
+    n_strings_list [ pvsam_model.SystemDesign.subarray1_nstrings, pvsam_model.SystemDesign.subarray2_nstrings, pvsam_model.SystemDesign.subarray3_nstrings, pvsam_model.SystemDesign.subarray4_nstrings ]
+    n_modules_per_string_list [ pvsam_model.SystemDesign.subarray1_modules_per_string, pvsam_model.SystemDesign.subarray2_modules_per_string, pvsam_model.SystemDesign.subarray3_modules_per_string, pvsam_model.SystemDesign.subarray4_modules_per_string ]
     for i in range(1, 4+1):
-        if i == 1 or pvsam_model.value(f'subarray{i}_enable') == 1:
-            n_modules += pvsam_model.value(f'subarray{i}_nstrings') \
-                       * pvsam_model.value(f'subarray{i}_modules_per_string')
+        if i == 1 or en_subarray_list[i+1] == 1:
+            n_modules += n_strings_list[i+1] * n_modules_per_string_list[i+1]
     return n_modules
 
 '''
 def get_modules_per_string(system_model) -> float:
     if isinstance(system_model, PVSamv1.Pvsamv1):
-        return system_model.value('subarray1_modules_per_string')
+        return system_model.SystemDesign.subarray1_modules_per_string
     else:
         return pv_module.modules_per_string
 
@@ -297,7 +301,7 @@ def set_cec_module_library_selection(model, module_name: str) -> dict:
     
     :returns: dictionary with variable values for selected module
     """
-    module_model = model.value('module_model')
+    module_model = model.Module.module_model
     if (module_model != 1):
         print("This function only works if module_model == 1 (CEC Performance Model with Module Database)")
         return
@@ -327,14 +331,14 @@ def set_cec_module_library_selection(model, module_name: str) -> dict:
 
 def set_cec_inverter_library_selection(model, inverter_name: str) -> dict:
     """
-    Return the inverter values from the CEC INverter Database library as a dictionary
+    Return the inverter values from the CEC Inverter Database library as a dictionary
     
-    :param model: Pvsamv1 model to write the module values to
-    :param module_name: Name of module for indexing library, str
+    :param model: Pvsamv1 model to write the inverter values to
+    :param inverter_name: Name of inverter for indexing library, str
     
     :returns: dictionary with variable values for selected inverter
     """
-    inv_model = model.value('inverter_model')
+    inv_model = model.Inverter.inverter_model
     if (inv_model != 0):
         print("This function only works if inverter_model == 1 (Inverter CEC Database)")
         return
@@ -348,18 +352,18 @@ def set_cec_inverter_library_selection(model, inverter_name: str) -> dict:
     CECInv = db[invfilter]
     CECParamList = CECInv.values.tolist()
     print(len(CECInv), " inverters selected. Name of 1st entry: ", CECInv.index[0])
-    if (len(CECMod) > 1):
-        print("More than 1 inverter found that matches the inverter name string. Please be more specific in the inverter name string.")
-        return
+    #if (len(CECInv) > 1):
+    #    print("More than 1 inverter found that matches the inverter name string. Please be more specific in the inverter name string.")
+    #    return
 
     column_names = list(CECInv.columns)
     unused_cols = ["inv_snl_ac_voltage", "inv_snl_idcmax"]
     for columnName, columnData in CECInv.items():
         if (columnName.startswith("inv_snl") and columnName not in unused_cols):
             if(columnName.startswith("inv_snl_mppt_low")):
-                model.value("mppt_low_inverter", columnData)
+                model.Inverter.mppt_low_inverter = columnData
             elif (columnName.startswith("inv_snl_mppt_hi")):
-                model.value("mppt_hi_inverter", columnData)
+                model.Inverter.mppt_hi_inverter = columnData
             else:
                 model.value(columnName, columnData)
         else:
